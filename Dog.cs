265f326c48582c3b9a14a2ec27c915b081643338
Michael Anderson <Micmail566@gmail.com>
2017-01-17 11:15:03 -0500

    Moved dog to its own class file.

---
+++ Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            var peanut = new Dog("Peanut");
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            peanut.HappyBirthday();
+            var age = peanut.GetAge();
+            var ageDogYears = peanut.GetAgeDogYears();
+            Console.WriteLine(age);
+            Console.WriteLine(ageDogYears);
+            Console.WriteLine(peanut.name);
+            Console.ReadLine();
+
+            var casey = new Dog("Casey");
+            var billy = new Dog("Billy");
+            var fido = new Dog("Fido");
+
+            var casey = new Dog("Casey");
+            peanut.RemoveFridend(fido);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,15 +11,29 @@ namespace DayTwo
     {
        private int Age;
         public string name;
+        public List<Dog> friends = new List<Dog>();
 
         public Dog(string _name)
         {
             Age = 0;
             name = _name;
         }
+        public void AddFriend(Dog friend)
+        {
+            if (friends.Contains(friend))
+                {
+                Console.WriteLine($"Dog {friend} is already our friend");
+
+            }
+            friends.Add(friend);
+        }
         public void HappyBirthday()
         {
             Age++;
+        }
+        public void RemoveFridend(Dog friend)
+        {
+            friends.Remove(friend);
         }
         public int GetAge()
         {
@@ -28,6 +42,15 @@ namespace DayTwo
         public int GetAgeDogYears()
         {
             return Age * 7;
+        }
+        public void GetFirend()
+        {
+
+        }
+        public void GetDogFirends()
+        {
+            friends.Add(friend);
+            Console.WriteLine(string.Join(",", friends));
         }
     }
     class Program
@@ -44,7 +67,9 @@ namespace DayTwo
             Console.WriteLine(age);
             Console.WriteLine(ageDogYears);
             Console.WriteLine(peanut.name);
+     
             Console.ReadLine();
+           
             
             {
                 
